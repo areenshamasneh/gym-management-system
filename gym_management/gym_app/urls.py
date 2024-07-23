7ee0d30b12434f3e7344f7d8e635905c0dd34278
Areen <109797692+areenyaran@users.noreply.github.com>
2024-07-23 15:10:27 +0300

    Add Api prefix

--- gym_management/gym_app/urls.py
+++ gym_management/gym_app/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views.controller_health_check import health_check
 
 urlpatterns = [
-    path('health/', health_check, name='health_check'),
+    path('api/health/', health_check, name='health_check'),
 ]
