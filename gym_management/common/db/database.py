96f856a048f33c569a7508801eb676fbfb8911af
Areen <109797692+areenyaran@users.noreply.github.com>
2024-09-04 15:31:57 +0300

    -

--- gym_management/common/db/database.py
+++ gym_management/common/db/database.py
@@ -1,10 +1,12 @@
 import logging
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
+
 from common.threads.thread import get_local
 from gym_management.settings import DATABASE_URL
 
-engine = create_engine(DATABASE_URL, pool_pre_ping=True, echo=True, echo_pool='debug')
+engine = create_engine(DATABASE_URL, pool_pre_ping=True, echo=True)
 
 SessionFactory = sessionmaker(autocommit=False, autoflush=False, bind=engine, expire_on_commit=False)
 
