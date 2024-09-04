ec12a0c8a4f458d22745cb1cc8eb224c2c599113
Areen <109797692+areenyaran@users.noreply.github.com>
2024-09-04 15:09:29 +0300

    session issue solved

--- gym_management/gym_app/middlewares/session_management.py
+++ gym_management/gym_app/middlewares/session_management.py
@@ -3,23 +3,8 @@ from common.db.database import Session
 
 
 class SessionManagementMiddleware(MiddlewareMixin):
-    @staticmethod
-    def process_request(request):
-        request.db_session = Session()
 
     @staticmethod
     def process_response(request, response):
-        try:
-            if 200 <= response.status_code < 400:
-                request.db_session.commit()
-        except Exception:
-            request.db_session.rollback()
-            raise
-        finally:
-            Session.remove()
-        return response
-
-    @staticmethod
-    def process_exception(request, exception):
-        request.db_session.rollback()
         Session.remove()
+        return response
