1239770ce357930769607841d3ab386871e95d9b
Areen <109797692+areenyaran@users.noreply.github.com>
2024-09-05 13:08:01 +0300

    -

--- gym_management/gym_app/repositories/hall_machine_repository.py
+++ gym_management/gym_app/repositories/hall_machine_repository.py
@@ -8,23 +8,21 @@ from gym_app.models.models_sqlalchemy import Hall, HallMachine
 class HallMachineRepository:
     @staticmethod
     def get_hall_machines_by_gym(gym_id):
-        with Session() as session:
-            query = (
-                select(HallMachine)
-                .join(Hall)
-                .filter(Hall.gym_id == gym_id)
-                .options(
-                    selectinload(HallMachine.hall).selectinload(Hall.gym),
-                    selectinload(HallMachine.hall).selectinload(Hall.hall_type),
-                    selectinload(HallMachine.machine)
-                )
+        query = (
+            select(HallMachine)
+            .join(Hall)
+            .filter(Hall.gym_id == gym_id)
+            .options(
+                selectinload(HallMachine.hall).selectinload(Hall.gym),
+                selectinload(HallMachine.hall).selectinload(Hall.hall_type),
+                selectinload(HallMachine.machine)
             )
-            result = session.execute(query)
-            return result.scalars().all()
+        )
+        result = Session.execute(query)
+        return result.scalars().all()
 
     @staticmethod
     def get_hall_machines_by_hall(hall_id):
-        with Session() as session:
-            query = select(HallMachine).filter(HallMachine.hall_id == hall_id)
-            result = session.execute(query)
-            return result.scalars().all()
+        query = select(HallMachine).filter(HallMachine.hall_id == hall_id)
+        result = Session.execute(query)
+        return result.scalars().all()
