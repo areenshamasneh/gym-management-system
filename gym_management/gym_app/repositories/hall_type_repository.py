5269acf437508444391f176ad52143d362f61824
Areen <109797692+areenyaran@users.noreply.github.com>
2024-09-05 11:49:35 +0300

    delete try and expect, Sitting management flow implementation in hall type code

--- gym_management/gym_app/repositories/hall_type_repository.py
+++ gym_management/gym_app/repositories/hall_type_repository.py
@@ -1,5 +1,6 @@
 from sqlalchemy import select, delete
 from sqlalchemy.exc import NoResultFound, IntegrityError
+from sqlalchemy.orm import joinedload
 
 from common.db.database import Session
 from gym_app.exceptions import DatabaseException
@@ -10,79 +11,65 @@ class HallTypeRepository:
 
     @staticmethod
     def get_all_hall_types():
-        with Session() as session:
+        try:
             query = select(HallType)
-            result = session.execute(query)
+            result = Session.execute(query)
             return result.scalars().all()
+        finally:
+            Session.remove()
 
     @staticmethod
     def get_hall_type_by_id(hall_type_id):
-        with Session() as session:
+        try:
             query = select(HallType).filter(HallType.id == hall_type_id)
-            try:
-                result = session.execute(query)
-                return result.scalar_one()
-            except NoResultFound:
-                return None
+            result = Session.execute(query)
+            return result.scalar_one_or_none()
+        finally:
+            Session.remove()
 
     @staticmethod
     def create_hall_type(data):
-        with Session() as session:
-            hall_type = HallType(**data)
-            try:
-                session.add(hall_type)
-                session.commit()
-                session.refresh(hall_type)
-                return hall_type
-            except IntegrityError as e:
-                session.rollback()
-                raise DatabaseException(f"Integrity error: {e.orig}")
+        hall_type = HallType(**data)
+        Session.add(hall_type)
+        return hall_type
 
     @staticmethod
     def update_hall_type(hall_type_id, data):
-        with Session() as session:
-            query = select(HallType).filter(HallType.id == hall_type_id)
-            hall_type = session.execute(query).scalar_one_or_none()
+        query = select(HallType).filter(HallType.id == hall_type_id)
+        hall_type = Session.execute(query).scalar_one_or_none()
 
-            if hall_type is None:
-                return None
+        if hall_type is None:
+            return None
 
-            if 'code' in data:
-                if data['code'] != hall_type.code:
-                    existing_hall_type_query = (
-                        select(HallType)
-                        .filter(HallType.code == data['code'])
-                        .filter(HallType.id != hall_type_id)
-                    )
-                    existing_hall_type = session.execute(existing_hall_type_query).scalar_one_or_none()
-                    if existing_hall_type:
-                        raise DatabaseException("Code already exists for another hall type.")
-                    hall_type.code = data['code']
+        if 'code' in data:
+            if data['code'] != hall_type.code:
+                existing_hall_type_query = (
+                    select(HallType)
+                    .filter(HallType.code == data['code'])
+                    .filter(HallType.id != hall_type_id)
+                )
+                existing_hall_type = Session.execute(existing_hall_type_query).scalar_one_or_none()
+                if existing_hall_type:
+                    raise DatabaseException("Code already exists for another hall type.")
+                hall_type.code = data['code']
 
-            if 'name' in data:
-                hall_type.name = data['name']
+        if 'name' in data:
+            hall_type.name = data['name']
 
-            if 'type_description' in data:
-                hall_type.type_description = data['type_description']
+        if 'type_description' in data:
+            hall_type.type_description = data['type_description']
 
-            try:
-                session.commit()
-                session.refresh(hall_type)
-                return hall_type
-            except IntegrityError as e:
-                session.rollback()
-                raise DatabaseException(f"Integrity error: {e.orig}")
+        Session.add(hall_type)
+        return hall_type
 
     @staticmethod
     def delete_hall_type(hall_type_id):
-        with Session() as session:
-            halls_query = select(Hall).filter(Hall.hall_type_id == hall_type_id)
-            associated_halls = session.execute(halls_query).scalars().all()
+        halls_query = select(Hall).filter(Hall.hall_type_id == hall_type_id)
+        associated_halls = Session.execute(halls_query).scalars().all()
 
-            if associated_halls:
-                raise DatabaseException("Cannot delete HallType because it is still in use by Halls.")
+        if associated_halls:
+            raise DatabaseException("Cannot delete HallType because it is still in use by Halls.")
 
-            query = delete(HallType).filter(HallType.id == hall_type_id)
-            result = session.execute(query)
-            session.commit()
-            return result.rowcount > 0
+        query = delete(HallType).filter(HallType.id == hall_type_id)
+        result = Session.execute(query)
+        return result.rowcount > 0
