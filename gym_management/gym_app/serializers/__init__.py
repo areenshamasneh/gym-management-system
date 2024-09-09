d8218c0fa74f0f680a6617ae047ec986cc445436
Areen <109797692+areenyaran@users.noreply.github.com>
2024-09-09 10:22:21 +0300

    solve init issue and delete unneeded packages

--- gym_management/gym_app/serializers/__init__.py
+++ gym_management/gym_app/serializers/__init__.py
@@ -1,2 +1,2 @@
-from gym_app.serializers.v1 import (AdminSchema, EmployeeSchema, MemberSchema, MachineSchema, HallMachineSchema,
-                                    HallSchema, GymSchema, HallTypeSchema)
+from gym_app.serializers.v1.serializers import AdminSchema, EmployeeSchema, MemberSchema, MachineSchema, \
+    HallMachineSchema, HallSchema, GymSchema, HallTypeSchema
--- gym_management/gym_app/serializers/v1/__init__.py
+++
@@ -1,2 +0,0 @@
-from gym_app.serializers.v1.serializers import AdminSchema, EmployeeSchema, MemberSchema, MachineSchema, \
-    HallMachineSchema, HallSchema, GymSchema, HallTypeSchema
